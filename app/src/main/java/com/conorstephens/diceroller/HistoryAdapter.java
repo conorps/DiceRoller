ad3bc1529af40c627f4613a11da047220a4bd4ca
Conor Stephens <conor.p.stephens@ibm.com>
2019-01-16 17:01:02 -0600

    Added functionality for dice rolling, history of dice rolls.

---
+++ app/src/main/java/com/conorstephens/diceroller/HistoryAdapter.java
@@ -0,0 +1,78 @@
+package com.conorstephens.diceroller;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHolder> {
+
+    private static final String TAG = HistoryAdapter.class.getSimpleName();
+    private ArrayList<JSONObject> mHistory;
+    private final Context mActivity;
+
+    public HistoryAdapter(Context context, ArrayList<JSONObject> mHistory) {
+        this.mActivity = context;
+        this.mHistory = mHistory;
+    }
+
+    /**
+     * This class represents each individual item in the RecyclerView
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        TextView diceNumber;
+        TextView result;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            cardView = itemView.findViewById(R.id.cardview_history);
+            diceNumber = itemView.findViewById(R.id.textview_history_dice);
+            result = itemView.findViewById(R.id.textview_history_result);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_item_roll, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    /**
+     * Binds the data in the mHistory ArrayList to the UI for each RecyclerView Item
+     * @param viewHolder The class that represents each item in the list
+     * @param index The index of the item in the ArrayList
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int index) {
+        final JSONObject jsonObject = mHistory.get(mHistory.size() - 1 -index);
+        try {
+            viewHolder.diceNumber.setText("D" + jsonObject.getString("dice"));
+            viewHolder.result.setText(jsonObject.getString("result"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        viewHolder.cardView.setTag(mHistory.size() - 1 - index);
+    }
+
+    /**
+     * Returns the size of the ArrayList
+     * @return
+     */
+    @Override
+    public int getItemCount() {
+        return mHistory.size();
+    }
+}
+
---
+++ app/src/main/java/com/conorstephens/diceroller/RollHistoryActivity.java
@@ -0,0 +1,105 @@
+package com.conorstephens.diceroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class RollHistoryActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private ArrayList<JSONObject> mHistory;
+    private HistoryAdapter mHistoryAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roll_history);
+
+        if (getSupportActionBar()!=null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        initRecyclerView();
+        getHistory();
+    }
+
+    /**
+     * Initializes the recyclerView that will store the rolls
+     */
+    private void initRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerview_history);
+        mRecyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(OrientationHelper.VERTICAL);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setLayoutManager(llm);
+
+        DividerItemDecoration myDivider = new DividerItemDecoration(this, llm.getOrientation());
+        myDivider.setDrawable(this.getResources().getDrawable(R.drawable.light_line));
+
+        mRecyclerView.addItemDecoration(myDivider);
+    }
+
+    /**
+     * Gets the history that was passed to this activity as a String, and converts the string to
+     * a JSONArray, and then calls getListOfJsonObject to convert the array to an arraylist of
+     * JSON objects. Sets the adapter for the recyclerView
+     */
+    private void getHistory() {
+        if (getIntent().hasExtra("rollHistory")) {
+            String historyString = getIntent().getStringExtra("rollHistory");
+            try {
+                JSONArray jsonArray = new JSONArray(historyString);
+                mHistory = getListOfJsonObject(jsonArray);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            if (mHistory != null) {
+                mHistoryAdapter = new HistoryAdapter(this, mHistory);
+                mRecyclerView.setAdapter(mHistoryAdapter);
+            }
+        }
+    }
+
+    /**
+     * Takes in a JSON array and returns an ArrayList of JSON objects. Each object in the array is
+     * added to the ArrayList
+     * @param jsonArray
+     * @return
+     */
+    public static ArrayList<JSONObject> getListOfJsonObject(
+            JSONArray jsonArray) {
+        ArrayList<JSONObject> list = new ArrayList<>();
+
+        if (jsonArray != null && jsonArray.length() > 0) {
+
+            for (int index = 0; index < jsonArray.length(); index++) {
+                JSONObject jsonObject = null;
+
+                try {
+                    jsonObject = jsonArray.getJSONObject(index);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                if (jsonObject != null) {
+                    list.add(jsonObject);
+                }
+            }
+        }
+        return list;
+    }
+
+}
